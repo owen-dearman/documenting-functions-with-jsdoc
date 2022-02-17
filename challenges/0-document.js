7e212a98e0b3a4b9471cfdbc9c2b2b7f362aa007
Owen Dearman <owendearman@gmail.com>
2022-02-17 11:58:37 +0000

    0-document.js JSDoc added

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,15 @@
-// TODO (Ex.0): document and write tests for this function
+/**
+ * Returns negative version of a given number.
+ * @param {number} n - number 
+ * @returns {number} the negative coeficient of n
+ */
 
 function asNegative(n) {
-  // ...
+  if (n > 0){
+  return n * -1;
+  } else {
+    return n;
+  }
 }
 
 
